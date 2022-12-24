f67204de0ac8a8b7a20d3346abdedc3cfa9cb1e8
lenbo <allenbora@gmail.com>
2022-12-25 02:51:07 +0800

    Eighteenth

---
+++ elifStatement.py
@@ -0,0 +1,14 @@
+#Using elif statement and determine or check the results.
+grade = 86
+
+if grade >= 90:
+  print("A")
+elif grade >= 80:
+  print("B")
+elif grade >= 70:
+  print("C")
+elif grade >= 60:
+  print("D")  
+else:
+  print("F")
+#END.....
